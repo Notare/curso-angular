003659d96b62f1383e1bb1ef96ec74fa503b1ada
Renato Sousa <renatonotare@gmail.com>
2024-09-11 10:37:56 -0300

    add aula 26

--- 04-diretivas/src/app/app.component.ts
+++ 04-diretivas/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DiretivaNgswitchComponent, TooltipModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
